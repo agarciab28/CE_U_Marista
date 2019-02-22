bbdb69ed506123e8d00ddf69ba8e8bd2c5068104
Alejandro Garcia Barajas <agarciab28@gmail.com>
2019-02-21 23:04:57 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Control-Escolar/resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    @yield('stylesheet')
+    <title>CE - @yield('title')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html>
---
+++ Control-Escolar/resources/views/login.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+  <link href="{{{ asset('css/style_login.css') }}}" rel="stylesheet">
+@endsection
+
+@section('title', 'Inicio de Sesion')
+
+@section('content')
+  <p>Prueba de vista</p>
+@endsection
