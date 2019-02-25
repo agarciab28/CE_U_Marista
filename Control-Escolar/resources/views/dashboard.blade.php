6e96d49b9201fba3f7b90ce7426adedfb9bb98b5
Takeshi <g.t.n.ramirez@gmail.com>
2019-02-24 20:35:43 -0600

    Login privisional
    Para usarlo correr migración y dar de alta usuarios

---
+++ Control-Escolar/resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+  <link href="{{{ asset('css/style_login.css') }}}" rel="stylesheet">
+@endsection
+
+@section('title', 'Home')
+
+@section('content')
+
+@endsection
