180ac7d76862fc7e729329ba3de4def78c3717f4
agarciab28 <agarciab28@gmail.com>
2019-03-02 14:30:27 -0600

    Auto stash before merge of "frontsinback" and "origin/frontsinback"

--- Control-Escolar/resources/views/layouts/app.blade.php
+++ Control-Escolar/resources/views/layouts/app.blade.php
@@ -13,6 +13,6 @@
     @yield('content')
   </body>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
+  <script src="{{{ asset('js/init.js') }}}"></script>
 
 </html>
