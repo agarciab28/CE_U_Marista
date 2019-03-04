59316db65ae4d2e8bbcf34b04dbf793ff3fde543
Takeshi <g.t.n.ramirez@gmail.com>
2019-03-03 19:34:26 -0600

    Migración de base de datos

---
+++ Control-Escolar/database/migrations/2019_03_04_005311_create_table_kardex.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableKardex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kardex', function (Blueprint $table) {
+            $table->string('ncontrol',10);
+            $table->text('obj_calificacion');
+            $table->string('intento');
+            $table->string('periodo');
+            //$table->timestamps();
+            $table->foreign('ncontrol')->references('ncontrol')->on('alumno');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kardex');
+    }
+}
