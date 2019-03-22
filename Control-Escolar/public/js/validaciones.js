1e0f9ed90071f71525727e614b9293d68ea3d027
Alexis Herrera <alex.herrera361@gmail.com>
2019-03-22 01:44:20 -0600

    Poquitas validaciones

--- Control-Escolar/public/js/validaciones.js
+++ Control-Escolar/public/js/validaciones.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  //Configuración del date picker
   $('.datepicker').datepicker({
     format: 'yyyy-mm-dd',
     setDefaultDate: true,
@@ -11,49 +12,59 @@ $(document).ready(function(){
       weekdaysAbbrev: ['D','L','M','M','J','V','S'],
     }
   });
+
+  //validaciones
+
+
+
+
+  $('#pb').submit(function(){
+    nombre = document.getElementById("nombre").value;
+    apellidop = document.getElementById("apellidop").value;
+    apellidom = document.getElementById("apellidom").value;
+    correo = document.getElementById("correo").value;
+    calle = document.getElementById("calle").value;
+    num_ext = document.getElementById("num_ext").value;
+    num_int = document.getElementById("num_int").value;
+    colonia = document.getElementById("colonia").value;
+    cp = document.getElementById("cp").value;
+    ciudad = document.getElementById("ciudad").value;
+    estado = document.getElementById("estado").value;
+    telefono = document.getElementById("telefono").value;
+    celular = document.getElementById("celular").value;
+      var exp_nom = /^[A-Za-zñÑ-áéíóúÁÉÍÓÚ\s\t-]+$/
+      var exp_correo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+
+      if(nombre == "" ||apellidop == ""|| apellidom == ""|| correo == ""|| calle == ""|| num_ext == ""|| num_int == ""|| colonia == ""|| cp == ""|| ciudad  == ""|| estado == ""|| telefono == ""|| celular == ""){
+        alert("Existen campos vacios");
+        return false;
+      } else if (!exp_nom.test(nombre)) {
+        alert("Formato de nombre incorrecto");
+        return false;
+      } else if (!exp_nom.test(apellidop)) {
+        alert("Formato de apellido paterno incorrecto");
+        return false;
+      } else if (!exp_nom.test(apellidom)) {
+        alert("Formato de apellido materno incorrecto");
+        return false;
+      } else if (!exp_correo.test(correo)) {
+        alert("Formato de correo incorrecto");
+        return false;
+      } else if (true) {
+
+      }
+
+  });
+
+
+/*
+  $("#nombre").keyup(function() {
+    var Max_Length = 5;
+    var length = $("#nombre").val().length;
+    if (length > Max_Length) {
+      $("#nombre").after("<p style='color:red'>the max length of "+Max_Length + " characters is reached, you typed in  " + length + "characters</p>");
+    }
+  });
+*/
+
 });
-
-//validaciones
-
-$('#registrar').click(function(){
-  alert("##");
-});
-function val(){
-  var nombre, variables, exp;
-
-  nombre = document.getElementById("nombre").value;
-  apellidop = document.getElementById("apellidop").value;
-  apellidom = document.getElementById("apellidom").value;
-  correo = document.getElementById("correo").value;
-  fecha = document.getElementById("fecha1").value;
-  calle = document.getElementById("calle").value;
-  num_ext = document.getElementById("num_ext").value;
-  num_int = document.getElementById("num_int").value;
-  colonia = document.getElementById("colonia").value;
-  cp = document.getElementById("cp").value;
-  ciudad = document.getElementById("ciudad").value;
-  estado = document.getElementById("estado").value;
-  telefono = document.getElementById("telefono").value;
-  celular = document.getElementById("celular").value;
-  pass = document.getElementById("pass").value;
-alert(fecha);
-  var exp_nom = /^[A-Za-zñÑ-áéíóúÁÉÍÓÚ\s\t-]+$/
-  var exp_correo = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
-
-  if(nombre == "" ||apellidop == ""|| apellidom == ""|| correo == ""|| fecha == ""|| calle == ""|| num_ext == ""|| num_int == ""|| colonia == ""|| cp == ""|| ciudad  == ""|| estado == ""|| telefono == ""|| celular == ""|| pass== ""){
-    alert("Existen campos vacios");
-    return false;
-  } else if (!exp_nom.test(nombre)) {
-    alert("Formato de nombre incorrecto");
-    return false;
-  } else if (!exp_nom.test(apellidop)) {
-    alert("Formato de apellido paterno incorrecto");
-    return false;
-  } else if (!exp_nom.test(apellidom)) {
-    alert("Formato de apellido materno incorrecto");
-    return false;
-  } else if (!exp_correo.test(correo)) {
-    alert("Formato de correo incorrecto");
-    return false;
-  }
-}
--- Control-Escolar/resources/views/admin/registrar.blade.php
+++ Control-Escolar/resources/views/admin/registrar.blade.php
@@ -11,7 +11,7 @@
       <div class="section container">
 
         <div class="row">
-          <form class="col  s12 m12" action="{{route('registro_persona')}}" method="post">
+          <form class="col  s12 m12" id="pb" action="{{route('registro_persona')}}" method="post">
             {{ csrf_field() }}
 
             <div class="row card-panel">
@@ -31,17 +31,17 @@
 
               <div class="input-field col m3 s12 ">
                 <!--<i class="material-icons prefix">account_circle</i>-->
-                <input type="text" name="nombres" id="nombre" class="validate" required>
+                <input type="text" name="nombres" id="nombre" class="validate" d>
                 <label for="nombre">Nombre:</label>
               </div>
               <div class="input-field col m3 s12 ">
                 <!--<i class="material-icons prefix">account_circle</i>-->
-                <input type="text" id="apellidop" name="apaterno" class="validate" required>
+                <input type="text" id="apellidop" name="apaterno" class="validate" >
                 <label for="apellidop">Apellido paterno:</label>
               </div>
               <div class="input-field col m3 s12 ">
                 <!--<i class="material-icons prefix">account_circle</i>-->
-                <input type="text" id="apellidom" name="amaterno" class="validate" required>
+                <input type="text" id="apellidom" name="amaterno" class="validate" >
                 <label for="apellidom">Apellido materno:</label>
               </div>
 
@@ -49,7 +49,7 @@
 
               <div class="input-field col m4 s12 ">
                 <i class="material-icons prefix">email</i>
-                <input type="text" id="correo" class="validate" name="email" required>
+                <input type="text" id="correo" class="validate" name="email" >
                 <label for="correo">Correo electrónico:</label>
               </div>
 
@@ -62,21 +62,21 @@
                   <i class="material-icons prefix">
                     wc
                   </i>
-                  <select name="sexo" required>
+                  <select name="sexo" >
                     <option value="" name="sexo" disabled selected>Sexo</option>
                     <option value="M">Masculino</option>
                     <option value="F">Femenino</option>
 
 
                   </select>
-                  <label>Tipo de usuario</label>
+                  <label>Sexo</label>
                 </div>
 
 
             <div class="input-field col m4 s12 ">
               <!--<i class="material-icons prefix">date_range</i>-->
               <label for="fecha">Fecha de nacimiento: </label>
-                <input type="text" name="fnaci" class="datepicker" required id="fecha1" >
+                <input type="text" name="fnaci" class="datepicker"  id="fecha1" >
             </div>
 
             <div class="row">
@@ -85,51 +85,52 @@
 
 
             <div class="input-field col m3 s12 ">
-              <input type="text" id="calle" name="calle" class="validate" required>
+              <input type="text" id="calle" name="calle" class="validate" >
               <label for="calle">Calle:</label>
             </div>
             <div class="input-field col m3 s12 ">
-              <input type="number" id="num_ext" name="num_ext" class="validate" required>
+              <input type="number" id="num_ext" name="num_ext" class="validate" >
               <label for="num_ext">Número exterior:</label>
             </div>
             <div class="input-field col m3 s12 ">
-              <input type="number" id="num_int" name="num_int" class="validate" required>
+              <input type="number" id="num_int" name="num_int" class="validate" >
               <label for="num_int">Número interior:</label>
             </div>
             <div class="input-field col m3 s12 ">
-              <input type="text" id="colonia" class="validate" name="colonia" required>
+              <input type="text" id="colonia" class="validate" name="colonia" >
               <label for="colonia">Colonia:</label>
             </div>
 
 
             <div class="input-field col m4 s12 ">
-              <input type="number" id="cp" class="validate" name="codigo_postal" required>
+              <input type="number" id="cp" class="validate" name="codigo_postal" >
               <label for="cp">Código postal:</label>
             </div>
             <div class="input-field col m4 s12 ">
-              <input type="text" id="ciudad" name="ciudad" class="validate" required>
+              <input type="text" id="ciudad" name="ciudad" class="validate" >
               <label for="ciudad">Ciudad:</label>
             </div>
             <div class="input-field col m4 s12 ">
-              <input type="text" id="estado" name="estado" class="validate" required>
+              <input type="text" id="estado" name="estado" class="validate" >
               <label for="estado">Estado:</label>
             </div>
 
             <div class="input-field col m6 s12 ">
-              <input type="tel" id="telefono" name="num_tel" class="validate" required>
+              <input type="tel" id="telefono" name="num_tel" class="validate" >
               <label for="telefono">Número de teléfono:</label>
             </div>
             <div class="input-field col m6 s12 ">
-              <input type="tel" id="celular" name="num_cel" class="validate" required>
+              <input type="tel" id="celular" name="num_cel" class="validate" >
               <label for="celular">Número de celular:</label>
             </div>
 
 
           <div class="input-field col m3 s12">
-            <button class="btn light-blue darken-4 waves-effect waves-light" type="submit" id="registrar">Registrar
+            <button class="btn light-blue darken-4 waves-effect waves-light" type="submit" >Registrar
               <i class="material-icons right">send </i>
             </button>
           </div>
+
           </form>
         </div>
       </div>
