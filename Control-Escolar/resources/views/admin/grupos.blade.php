89d15dcc13182ccfbd7cbb6f608abb00c81312ca
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-03-24 18:17:32 -0600

    Grupos 2%

--- Control-Escolar/resources/views/admin/grupos.blade.php
+++ Control-Escolar/resources/views/admin/grupos.blade.php
@@ -15,11 +15,11 @@
   <div class="container">
     <div class="row">
       <div class="col s12 m12 l12">
-        <h5 class="breadcrumbs-title">Grupos</h5>
+        <h5 class="breadcrumbs-title">Crear grupo</h5>
         <ol class="breadcrumbs">
             <li><a href="/admin">Inicio</a></li>
             <li><a href="#">Grupos</a></li>
-            <li class="active">...</li>
+            <li class="active">Crear grupo</li>
         </ol>
       </div>
     </div>
--- Control-Escolar/resources/views/admin/listas/grupos.blade.php
+++ Control-Escolar/resources/views/admin/listas/grupos.blade.php
@@ -8,6 +8,21 @@
 @section('title', 'Grupos')
 
 @section('content')
-
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <h5 class="breadcrumbs-title">Listar grupos</h5>
+        <ol class="breadcrumbs">
+            <li><a href="/admin">Inicio</a></li>
+            <li><a href="#">Grupos</a></li>
+            <li class="active">Listar grupo</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!--breadcrumbs end-->
 
 @endsection
