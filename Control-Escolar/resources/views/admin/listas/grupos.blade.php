900f001bc470b9802a6d5d0b2946a6dcd4ef2f7e
agarciab28 <agarciab28@gmail.com>
2019-04-16 17:38:48 -0500

    cambio ids forms

--- Control-Escolar/resources/views/admin/listas/grupos.blade.php
+++ Control-Escolar/resources/views/admin/listas/grupos.blade.php
@@ -48,58 +48,61 @@
   <!--modal modificar grupos-->
   <div id="modal_modificar" class="modal bottom-sheet">
       <div class="modal-content">
-        <form class="col  s12 m12" id="" action="{{route('admin_registrar_Grupos')}}" method="post">
-        {{ csrf_field() }}
-              <div class="row">
-                <div class="col m6 push-m3 s12" style="text-align: center;">
-                  <h4>Modificar Grupo</h4>
-                </div>
-              </div>
-              <div class="input-field col s12 m3">
-                <input type="text" name="idgrupo" id="idgrupo" value="" disabled>
-                <label for="idgrupo">Identificador</label>
-              </div>
-                <div class="input-field col s12 m3">
-                  <input type="text" name="seccion" id="seccion" value="">
-                  <label for="seccion">Seccion</label>
-                </div>
-                <div class="input-field col s12 m3">
-                  <select class="" name="carrera" id="carrera">
-                    <option value="" disabled>Elige una opcion</option>
-                  
-                  </select>
-                  <label for="carrera">Carrera</label>
-                </div>
-                <div class="input-field col s12 m3">
-                  <select class="" name="materia" id="materia">
-                    <option value="" disabled>Elige una opcion</option>
-
-                  </select>
-                  <label for="materia">Materia</label>
-                </div>
-                <div class="input-field col s12 m6">
-                  <select class="" name="profesor" id="profesor">
-                    <option value="" disabled>Elige una opcion</option>
-
-                  </select>
-                  <label for="profesor">Profesor</label>
-                </div>
-                <div class="input-field col s12 m6">
-                  <input type="text" name="periodo" id="periodo" value="">
-
-                  </select>
-                  <label for="periodo">Periodo</label>
-                </div>
-
+        <div class="row">
+          <form class="col  s12 m12" id="" action="{{route('admin_registrar_Grupos')}}" method="post">
+          {{ csrf_field() }}
                 <div class="row">
+                  <div class="col m6 push-m3 s12" style="text-align: center;">
+                    <h4>Modificar Grupo</h4>
+                  </div>
                 </div>
+                <div class="input-field col s12 m3">
+                  <input type="text" name="idgrupo" id="idgrupo" value="" disabled>
+                  <label for="idgrupo">Identificador</label>
+                </div>
+                  <div class="input-field col s12 m3">
+                    <input type="text" name="seccion" id="seccion" value="">
+                    <label for="seccion">Seccion</label>
+                  </div>
+                  <div class="input-field col s12 m3">
+                    <select class="" name="carrera" id="carrera">
+                      <option value="" disabled>Elige una opcion</option>
 
-                <div class="input-field col m3 s12">
-                    <button class="btn light-blue darken-4" type="submit">Registrar
-                        <i class="material-icons right">send </i>
-                    </button>
-                </div>
-        </form>
+                    </select>
+                    <label for="carrera">Carrera</label>
+                  </div>
+                  <div class="input-field col s12 m3">
+                    <select class="" name="materia" id="materia">
+                      <option value="" disabled>Elige una opcion</option>
+
+                    </select>
+                    <label for="materia">Materia</label>
+                  </div>
+                  <div class="input-field col s12 m6">
+                    <select class="" name="profesor" id="profesor">
+                      <option value="" disabled>Elige una opcion</option>
+
+                    </select>
+                    <label for="profesor">Profesor</label>
+                  </div>
+                  <div class="input-field col s12 m6">
+                    <input type="text" name="periodo" id="periodo" value="">
+
+                    </select>
+                    <label for="periodo">Periodo</label>
+                  </div>
+
+                  <div class="row">
+                  </div>
+
+                  <div class="input-field col m3 s12">
+                      <button class="btn light-blue darken-4" type="submit">Registrar
+                          <i class="material-icons right">send </i>
+                      </button>
+                  </div>
+          </form>
+        </div>
+
       </div>
       <div class="modal-footer">
           <a href="#!" class="modal-close waves-effect waves-green btn-flat">
