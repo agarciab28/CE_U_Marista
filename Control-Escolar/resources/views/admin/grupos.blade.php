f60cb6269a6d0bae34580099f41596665d042a81
agarciab28 <agarciab28@gmail.com>
2019-04-18 22:05:18 -0500

    alumnos por materia

--- Control-Escolar/resources/views/admin/grupos.blade.php
+++ Control-Escolar/resources/views/admin/grupos.blade.php
@@ -68,6 +68,11 @@
 
                     <label for="periodo">Periodo</label>
                   </div>
+                  <div class="row">
+
+                  </div>
+
+                  <a href="#modal_asignar" class="btn modal-trigger">Asignar Alumnos</a>
 
                   <div class="row">
                   </div>
@@ -80,6 +85,53 @@
               </div>
           </form>
       </div>
+  </div>
+
+  <!-- Modal asignar -->
+  <div id="modal_asignar" class="modal">
+    <div class="modal-content">
+      form class="" action="" method="post">
+
+        <div class="filtros">
+            <div class="row">
+                      <div class="col m6 push-m3 s12" style="text-align: center;">
+                        <h4>Asignación de alumnos a grupo</h4>
+                      </div>
+            </div>
+          <div class="row">
+
+
+            <div class="input-field col m6 s12 ">
+
+              <label for="fcarrera">Carrera:  aqui iria la consulta de carrera</label>
+            </div>
+
+            <div class="input-field col m6 s12 ">
+              <input type="number" name="fperiodo" id="fperiodo" value="1" max="12" min="1" class="validate" required maxlength="35">
+              <label for="fperiodo">Semestre</label>
+            </div>
+          </div>
+        </div>
+
+
+        <table id="example" class="responsive-table striped" style="width:100%">
+              <thead>
+                  <tr>
+                      <th>Seleccionar</th>
+                      <th>Número de control</th>
+                      <th>Nombre Completo</th>
+                      <th>Fecha de Nacimiento</th>
+                      <th>Correo Electronico</th>
+                  </tr>
+              </thead>
+              <tbody>
+              </tbody>
+          </table>
+          <div class="input-field">
+            <button type="button" class="btn modal-close" name="button">Registrar</button>
+          </div>
+      </form>
+    </div>
   </div>
 @endsection
 
