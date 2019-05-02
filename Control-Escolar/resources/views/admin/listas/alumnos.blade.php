8f703daba2e9fb8d703654449c931adf31fab249
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-05-01 23:35:01 -0500

    SwetAlert para deshabilitar alumnos

--- Control-Escolar/resources/views/admin/listas/alumnos.blade.php
+++ Control-Escolar/resources/views/admin/listas/alumnos.blade.php
@@ -2,12 +2,12 @@
 
 @section('stylesheet')
 
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.0/material.min.css" rel="stylesheet">
-  <link href="https://cdn.datatables.net/1.10.19/css/dataTables.material.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.0/material.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.19/css/dataTables.material.min.css" rel="stylesheet">
 
-  <link href="{{{ asset('css/style_dashboard.css') }}}" rel="stylesheet">
-  <link href="{{{ asset('css/admin/usuarios.css') }}}" rel="stylesheet">
-  <link href="{{{ asset('css/admin/alumnos.css') }}}" rel="stylesheet">
+<link href="{{{ asset('css/style_dashboard.css') }}}" rel="stylesheet">
+<link href="{{{ asset('css/admin/usuarios.css') }}}" rel="stylesheet">
+<link href="{{{ asset('css/admin/alumnos.css') }}}" rel="stylesheet">
 
 @endsection
 
@@ -16,34 +16,36 @@
 @section('content')
 <div class="container">
   <table id="example" class="responsive-table striped" style="width:100%">
-        <thead>
-            <tr>
-                <th>Nombre de Usuario</th>
-                <th>Nombre Completo</th>
-                <th>Fecha de Nacimiento</th>
-                <th>Correo Electronico</th>
-                <th>Modificar</th>
-                <th>Deshabilitar</th>
-            </tr>
-        </thead>
-        <tbody>
-          @foreach($personas as $persona)
-            <tr>
-                <td>{{$persona->ncontrol}}</td>
-                <td>{{$persona->nombres}} {{$persona->apaterno}} {{$persona->amaterno}}</td>
-                <td>{{$persona->fnaci}}</td>
-                <td>{{$persona->email}}</td>
-                <td> <a href="{{ route('admin_musuarios') }}" class="btn">Modificar</a> </td>
-                <td> <a href="#" class="btn red">Deshabilitar</a> </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Nombre de Usuario</th>
+        <th>Nombre Completo</th>
+        <th>Fecha de Nacimiento</th>
+        <th>Correo Electronico</th>
+        <th>Modificar</th>
+        <th>Deshabilitar</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($personas as $persona)
+      <tr>
+        <td>{{$persona->ncontrol}}</td>
+        <td>{{$persona->nombres}} {{$persona->apaterno}} {{$persona->amaterno}}</td>
+        <td>{{$persona->fnaci}}</td>
+        <td>{{$persona->email}}</td>
+        <td> <a href="{{ route('admin_musuarios') }}" class="btn">Modificar</a> </td>
+        <td> <a href="#" class="btn green" id="habilitar">Habilitado</a> </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
 @endsection
 @section('scripts')
-<script src="{{{ asset('js/datatables.js') }}}"></script>
-
+<!--sweetalert -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="{{{ asset('js/datatables.js')}}}"></script>
+<script src="{{{ asset('js/funciones.js')}}}"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.material.min.js"></script>
