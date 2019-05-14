6f07f86d355f7cc324a3e97945cc9a01af708385
Takeshi <g.t.n.ramirez@gmail.com>
2019-05-14 18:24:04 -0500

    Gráfica alumnos-carrera

--- Control-Escolar/app/Http/Controllers/graficasController.php
+++ Control-Escolar/app/Http/Controllers/graficasController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class graficasController extends Controller
 {
-    //
+    public function alumnosCarrera(){
+      $alumnos=DB::select('select count(*) as cantidad , nombre_carrera from alumno as a inner join carrera as c on a.id_carrera=c.id_carrera group by nombre_carrera');
+        dd($alumnos);
+    }
+    public function show(){
+      $alumnos=DB::select('select count(*) as cantidad , nombre_carrera from alumno as a inner join carrera as c on a.id_carrera=c.id_carrera group by nombre_carrera');
+       return view('admin.estadisticas',compact(['alumnos']));
+    }
 }
