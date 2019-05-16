4bdd7a2f38772e6790022626ad4fc8e332d37896
Takeshi <g.t.n.ramirez@gmail.com>
2019-05-16 08:00:52 -0500

    Fix a horarios por alumno, no estoy seguro de que sirva

--- Control-Escolar/app/Http/Controllers/alumnohController.php
+++ Control-Escolar/app/Http/Controllers/alumnohController.php
@@ -17,65 +17,65 @@ class alumnohController extends Controller
 {
     public function showHorarios($idp){
         $nombres=persona::select('nombres')->where('id_persona',$idp)->value('nombres');
-        $horariolu=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_lu','hora_i_lu','hora_f_lu', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horariolu=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_lu','hora_i_lu','hora_f_lu',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_lu')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
-        $horarioma=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_ma','hora_i_ma','hora_f_ma', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horarioma=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_ma','hora_i_ma','hora_f_ma',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_ma')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
-        $horariomi=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_mi','hora_i_mi','hora_f_mi', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horariomi=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_mi','hora_i_mi','hora_f_mi',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_mi')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
-        $horarioju=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_ju','hora_i_ju','hora_f_ju', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horarioju=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_ju','hora_i_ju','hora_f_ju',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_ju')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
-        $horariovi=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_vi','hora_i_vi','hora_f_vi', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horariovi=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_vi','hora_i_vi','hora_f_vi',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_vi')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
-        $horariosa=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_sa','hora_i_sa','hora_f_sa', 
-        'grupo.activo as activo','c.id_carrera as id_carrera')
+        $horariosa=grupo::select('grupo.id_grupo as grupo','nombre_materia','nombres','apaterno','amaterno','aula_sa','hora_i_sa','hora_f_sa',
+        'grupo.activo as activo','grupo.id_carrera as id_carrera')
         ->join('materia as m','m.id_materia','=','grupo.id_materia')
         ->join('profesor as p','p.id_prof','=','grupo.id_prof')
         ->join('personal as per','per.username','p.username')
         ->join('persona as pe','pe.id_persona','=','per.id_persona')
         ->join('horario as h','h.id_grupo','=','grupo.id_grupo')
         ->whereNotNull('hora_i_sa')
-        ->where('persona.nombres',$nombres)
+        ->where('pe.nombres',$nombres)
         ->get();
        return view('alumno.grupos',compact(['horariolu','horarioma','horariomi','horarioju','horariovi','horariosa']));
      }
