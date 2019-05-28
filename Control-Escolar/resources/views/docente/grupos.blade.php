f2df000a10887641f8f7a0094782c9b7fddf67f0
AlexTremo08 <alex.herrera361@gmail.com>
2019-05-28 00:58:02 -0500

    pa ke veas

--- Control-Escolar/resources/views/docente/grupos.blade.php
+++ Control-Escolar/resources/views/docente/grupos.blade.php
@@ -55,6 +55,26 @@ $grupo_seleccionado=0;
                 <input type="text" name="id_grupo" value="" hidden>
                 <button type="submit" class="collection-item black-text botonModal">Calificaciones Finales</button>
               </form>
+              <form class="" action="{{ route('docente_pdfA') }}" method="post">
+                @csrf
+                <input type="text" name="id_grupo" value="" hidden>
+                <button type="submit" class="collection-item black-text botonModal">PDF BOLETA DE CALIFICACIONES: EVALUACIÓN ORDINARIA</button>
+              </form>
+              <form class="" action="#" method="post">
+                @csrf
+                <input type="text" name="id_grupo" value="" hidden>
+                <button type="submit" class="collection-item black-text botonModal">PDF BOLETA DE CALIFICACIONES: PRIMERA EVALUACIÓN EXTRAORDINARIA</button>
+              </form>
+              <form class="" action="#" method="post">
+                @csrf
+                <input type="text" name="id_grupo" value="" hidden>
+                <button type="submit" class="collection-item black-text botonModal">PDF RELACIÓN DE ALUMNOS NO APROBADOS</button>
+              </form>
+              <form class="" action="#" method="post">
+                @csrf
+                <input type="text" name="id_grupo" value="" hidden>
+                <button type="submit" class="collection-item black-text botonModal">PDF BOLETA DE CALIFICACIONES FINALES</button>
+              </form>
               <!--<form class="" action="" method="post">
                 @csrf
                 <input type="text" name="id_grupo" value="" hidden>
